56d242482945410bdfd8f3cebb58dda9deee327b
Amr <legithamouda@gmail.com>
2025-06-15 21:00:31 +0300

    Add exercise 1.9

--- part1/unicafe/unicafe-app/src/App.jsx
+++ part1/unicafe/unicafe-app/src/App.jsx
@@ -1,6 +1,16 @@
 import { useState } from 'react'
 
 function Statistics({good, bad, neutral}){
+  if(good === 0 && bad === 0 && neutral === 0){
+    return(
+      <div className='statistics'>
+        <h2> Statistics </h2>
+        <p> No feedback given </p>
+      </div>
+    )
+
+  }else{
+
   return(
   <div className='statistics'>
       <h2> Statistics </h2>
@@ -12,13 +22,14 @@ function Statistics({good, bad, neutral}){
       <p> positive {good *(100/(good+ neutral+bad))}%</p> 
       </div>
   )
+  }
 }
 
 function App() {
   //save clicks of each button to its own state 
-  const [good,setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
+  const [good,setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   function handleGoodClick(){
     setGood(good + 1)
