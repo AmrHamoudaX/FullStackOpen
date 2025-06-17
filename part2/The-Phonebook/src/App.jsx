29af5f65c45a98f861708349ee7e181e1f8154aa
Amr <legithamouda@gmail.com>
2025-06-17 12:16:23 +0300

    Adds exercise 2.8

--- part2/The-Phonebook/src/App.jsx
+++ part2/The-Phonebook/src/App.jsx
@@ -6,14 +6,16 @@ function App() {
     {name: 'Arto Hellas'}
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   function handleSubmit(e){
     e.preventDefault()
    if(persons.find(person => person.name === newName)){
       alert(`${newName} is already added to Phonebook`)
     }else{
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({name: newName,number: newNumber}))
       setNewName('')
+      setNewNumber('')
     }
 
   }
@@ -22,12 +24,18 @@ function App() {
     setNewName(e.target.value)
   }
 
+  function handleNewNumber(e){
+    setNewNumber(e.target.value)
+
+  }
+
   return(
   <div>
       <h2> Phonebook </h2>
       <form onSubmit={handleSubmit}>
         <div>
         name: <input value={newName} onChange={handleNewName}/>
+        number: <input value={newNumber} onChange={handleNewNumber}/>
         </div>
         <div>
         <button type='submit'> add </button>
@@ -36,7 +44,7 @@ function App() {
       <h2>Number</h2>
 
       {persons.map(person=> 
-        <li key={person.name}> {person.name} </li>
+        <li key={person.name}> {person.name}: {person.number} </li>
       )}
 
 
