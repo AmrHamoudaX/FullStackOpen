1718c04ba6e5549e2f87b1e7ca89c0a9cec5d019
Amr <legithamouda@gmail.com>
2025-06-25 15:32:34 +0300

    Adds exercise 2.20
    Adds the weather report for the capital of the country

--- part2/Data-For-Countries/src/App.jsx
+++ part2/Data-For-Countries/src/App.jsx
@@ -6,13 +6,29 @@ function App() {
   const [chosenCountry, setChosenCountry] = useState(null)
   const [allCountries, setAllCountries] = useState(null)
   const [searchValue, setSearchValue] = useState('')
+  const [geoCode, setGeoCode] = useState(null)
+  const [weatherData, setWeatherData ] = useState(null)
+  const api_key = import.meta.env.VITE_SOME_KEY
 
   const showCountries = allCountries
     ? allCountries.filter(country=> 
     country.slice(0,searchValue.length).toLowerCase() === searchValue.toLowerCase())
     : null
 
+    //Get WeatherData 
+  useEffect(()=> {
+    if(chosenCountry){
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCountry.capital}&appid=${api_key}`
+      axios
+      .get(weatherUrl)
+      .then(response=> {
+          const data = response.data
+          setWeatherData({wind: data.wind.speed, icon:`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,tempInC:(data.main.temp-273.15).toFixed(2) ,lat:data.coord.lat, lon:data.coord.lon})
+        })
+    }
+  },[chosenCountry])
 
+    //Get Country's Data
   useEffect(()=> {
     if(showCountries && showCountries.length ===1 ){
       axios .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${showCountries[0].toLowerCase()}`)
@@ -76,6 +92,15 @@ function App() {
         })}
       </ul>
           <img src={chosenCountry.flag} />
+          <h2> Weather in {chosenCountry.capital} </h2>
+          {weatherData
+          ? <>
+              <p> Temperature {weatherData.tempInC} Celsius </p>
+              <img src={weatherData.icon}/>
+              <p> Wind {weatherData.wind} m/s </p>
+            </>
+          : null
+          }
       </>
       :null
       } 
